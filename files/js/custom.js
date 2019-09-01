d543d5e2e684ca013ecb248795ec2553ea3334c1
Amid Hasanov <amidah@code.edu.az>
2019-09-01 23:02:22 +0400

    index page 90% ready

---
+++ files/js/custom.js
@@ -0,0 +1,175 @@
+(function ($) {
+	
+	"use strict";
+
+	// Header Scrolling Set White Background
+	scrollNavBar();
+
+	// Window Resize Mobile Menu Fix
+	mobileNav();
+
+
+	// Scroll animation init
+	window.sr = new scrollReveal();
+	
+
+	// Menu Dropdown Toggle
+	if($('.menu-details').length){
+		$(".menu-details").on('click', function() {	
+			$(this).toggleClass('active');
+			$('.header-section .nav').slideToggle(200);
+		});
+	}
+
+
+	// Menu elevator animation
+	$('a[href*=\\#]:not([href=\\#])').on('click', function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				var width = $(window).width();
+				if(width < 991) {
+					$('.menu-details').removeClass('active');
+					$('.header-section .nav').slideUp(200);	
+				}				
+				$('html,body').animate({
+					scrollTop: (target.offset().top) - 30
+				}, 700);
+				return false;
+			}
+		}
+	});
+
+
+
+	// Home number counterup
+	if($('.count-item').length){
+		$('.count-item strong').counterUp({
+			delay: 10,
+			time: 1000
+		});
+	}
+
+
+	// Blog cover image
+	if($('.what-is-blog-news').length){
+		$('.what-is-blog-news .img').imgfix();
+	}
+
+
+	// About Us Image
+	if($('.about-image').length){
+		$('.about-image').imgfix({
+			scale: 1.1
+		});
+	}
+
+
+	// Sidebar contact banner image
+	if($('.sidebar .box').length) {
+		$('.sidebar .box').imgfix();
+	}
+
+
+	// Home Us Image
+	if($('.home-pic').length){
+		$('.home-pic').imgfix({
+			scale: 1.1
+		});
+	}
+
+
+	// Page standard gallery
+	if($('.page-gallery').length && $('.page-gallery-wrapper').length){
+		$('.page-gallery').imgfix({
+			scale: 1.1
+		});
+
+		$('.page-gallery').magnificPopup({
+			type: 'image',
+			gallery: {
+				enabled: true
+			},
+			zoom: {
+				enabled: true,
+				duration: 300,
+				easing: 'ease-in-out',
+			}
+		});
+	}
+
+
+	// Page loading animation
+	$(window).on('load', function() {
+		if($('.cover').length){
+			$('.cover').parallax({
+				imageSrc: $('.cover').data('image'),
+				zIndex: '1'
+			});
+		}
+
+		$("#preloader").animate({
+			'opacity': '0'
+		}, 600, function(){
+			setTimeout(function(){
+				// Home Parallax
+				if($('#parallax-text').length){
+					$('#parallax-text').parallax({
+						imageSrc: 'files/images/parallax.jpg',
+						zIndex: '1'
+					});
+				}
+
+				// Home Parallax Counter
+				if($('#counter').length){
+					$('#counter').parallax({
+						imageSrc: 'files/images/parallax.jpg',
+						zIndex: '1'
+					});
+				}
+				$("#preloader").css("visibility", "hidden").fadeOut();
+			}, 300);
+		});
+	});
+
+
+	// Header Scrolling Set White Background
+	$(window).on('scroll', function() {
+		scrollNavBar();
+	});
+
+
+	// Window Resize Mobile Menu Fix
+	$(window).on('resize', function() {
+		mobileNav();
+	});
+
+
+	// Window Resize Mobile Menu Fix
+	function mobileNav() {
+		var width = $(window).width();
+		$('.submenu').on('click', function() {
+			if(width < 992) {
+				$('.submenu ul').removeClass('active');
+				$(this).find('ul').toggleClass('active');
+			}
+		});
+	}
+
+
+	// Navbar Scroll Set White Background Function
+	function scrollNavBar() {
+		var width = $(window).width();
+		if(width > 991) {
+			var scroll = $(window).scrollTop();
+			if (scroll >= 30) {
+				$(".header-section").addClass("header-sticky");
+			}else{
+				$(".header-section").removeClass("header-sticky");
+			}
+		}
+	}
+
+
+})(window.jQuery);
